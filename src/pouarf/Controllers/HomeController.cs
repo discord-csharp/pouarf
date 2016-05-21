060bc4eba4a475f358469d65ae1010a4dafd2017
@mariocatch <mariocatch@gmail.com>
2016-05-20 23:55:38 -0400

    Initial commit.

---
+++ src/pouarf/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pouarf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/pouarf/wwwroot/app/person/person.component.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('pouarf.person')
+        .component('person', {
+            templateUrl: 'app/person/person.component.html',
+            controller: PersonController,
+            controllerAs: 'vm'
+        });
+
+    function PersonController() {
+        var vm = this;
+        vm.name = '';
+
+        activate();
+
+        function activate() {
+            console.log('PersonComponent activated!')
+        }
+    }
+})();
