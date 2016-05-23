2b5b7de554d0fbf80a42c5c0d40a61c7919f8a16
@mariocatch <mariocatch@gmail.com>
2016-05-22 22:30:11 -0400

    Add people api consumption service. Add person-list component.

---
+++ src/pouarf/wwwroot/app/person/person-list.component.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('pouarf.person')
+        .component('personList', {
+            templateUrl: 'app/person/person-list.component.html',
+            controller: ['people.service', PersonController],
+            controllerAs: 'vm'
+        });
+
+    function PersonController(PeopleService) {
+        var vm = this;
+        vm.people = [];
+
+        activate();
+
+        function activate() {
+            return PeopleService.getPeople()
+                .then(function (data) {
+                    vm.people = data;
+                    return vm.people;
+                });
+        }
+    }
+})();
